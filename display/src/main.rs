776e41b5508358bdecac10dfba3cb888d750e271
riteshco <riteshgadwalv2@gmail.com>
2026-01-08 21:40:19 +0530

    Add missed opcode handlers, input mapping and complete the loop
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>

--- display/src/main.rs
+++ display/src/main.rs
@@ -7,8 +7,9 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::keyboard::Keycode;
 
-const SCALE: u32 = 15;
+const SCALE: u32 = 20;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 const TICKS_PER_FRAME: u32 = 10;
@@ -30,6 +31,28 @@ fn draw_screen(emu: &Emu , canvas: &mut Canvas<Window>) {
         }
     }
     canvas.present();
+}
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q    => Some(0x4),
+        Keycode::W    => Some(0x5),
+        Keycode::E    => Some(0x6),
+        Keycode::R    => Some(0xD),
+        Keycode::A    => Some(0x7),
+        Keycode::S    => Some(0x8),
+        Keycode::D    => Some(0x9),
+        Keycode::F    => Some(0xE),
+        Keycode::Z    => Some(0xA),
+        Keycode::X    => Some(0x0),
+        Keycode::C    => Some(0xB),
+        Keycode::V    => Some(0xF),
+        _             =>      None,
+    }
 }
 
 fn main() {
@@ -66,7 +89,17 @@ fn main() {
     'gameloop: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} => break 'gameloop,
+                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'gameloop,
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k , true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k , false);
+                    }
+                },
                 _ => ()
             }
         }
